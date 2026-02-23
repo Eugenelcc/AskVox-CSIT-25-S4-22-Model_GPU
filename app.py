f960c69fe9adf5ad8e3ab7ead8403aefbd2e0a10
Justin <166772926+Cakebut@users.noreply.github.com>
2026-02-23 22:28:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ LORA_GGUF = {
 # -----------------------
 N_CTX = int(os.getenv("N_CTX", "8192"))
 N_THREADS = int(os.getenv("N_THREADS", "16"))
-N_GPU_LAYERS = int(os.getenv("N_GPU_LAYERS", "80"))
+N_GPU_LAYERS = int(os.getenv("N_GPU_LAYERS", "1"))
 
 # -----------------------
 # System prompt
